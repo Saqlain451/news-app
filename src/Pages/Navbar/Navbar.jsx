2ae6e7b7e771cde1b646d4dd34c6dc015dd0bf55
Saqlain451 <jobs.sakil.mustak@gmail.com>
2023-04-18 16:16:29 +0530

    search btn added

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import { HiMoon } from 'react-icons/hi';
-import { NavLink } from 'react-router-dom';
-import logo from '../../assets/logo1.png'
-import { useGlobalHook } from '../../Hooks/Context';
-import { FaSun } from 'react-icons/fa'
-import "./navbar.css"
+import React from "react";
+import { HiMoon } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo1.png";
+import { useGlobalHook } from "../../Hooks/Context";
+import { FaSun, FaSearch } from "react-icons/fa";
+import "./navbar.css";
 const Navbar = () => {
   const { themeChangeHandle, isDark } = useGlobalHook();
   return (
     <header className="header">
       <div className="navbar-logo">
-        <NavLink to="/"><img src={logo} alt="logo" />SNews</NavLink>
+        <NavLink to="/">
+          <img src={logo} alt="logo" />
+          SNews
+        </NavLink>
       </div>
-      <span onClick={themeChangeHandle}> {isDark ? <FaSun /> : <HiMoon />}</span>
+      <div className="nav-btns">
+        <span>
+          <NavLink to={"/search"}>
+            <FaSearch />
+          </NavLink>
+        </span>
+        <span onClick={themeChangeHandle}>
+          {isDark ? <FaSun /> : <HiMoon />}
+        </span>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
